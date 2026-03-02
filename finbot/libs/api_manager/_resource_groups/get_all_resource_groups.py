42920492ddac46e7930af477b8e75de67e7167b7
Jeremy Dawson <jeremyjdawson@gmail.com>
2026-03-02 14:05:56 -0500

    rescue: dev-laptop localsync migration snapshot 2026-03-02T14:05:56-05:00

--- finbot/libs/api_manager/_resource_groups/get_all_resource_groups.py
+++ finbot/libs/api_manager/_resource_groups/get_all_resource_groups.py
@@ -1,12 +1,14 @@
 """Aggregates all pre-configured resource group instances for bulk registration."""
 
 from finbot.libs.api_manager._resource_groups.api_resource_groups import (
+    alpaca_api_resource_group,
     alpha_vantage_api_resouce_group,
     alpha_vantage_rapidapi_resouce_group,
     bureau_of_labour_statistics_api_2_resource_group,
     fred_api_resource_group,
     nasdaq_quandl_api_resource_group,
     oanda_api_resource_group,
+    twelvedata_api_resource_group,
 )
 from finbot.libs.api_manager._utils.api_resource_group import APIResourceGroup
 
@@ -21,9 +23,11 @@ def get_all_resource_groups() -> dict[str, APIResourceGroup]:
     resource_groups = [
         alpha_vantage_api_resouce_group,
         alpha_vantage_rapidapi_resouce_group,
+        alpaca_api_resource_group,
         bureau_of_labour_statistics_api_2_resource_group,
         fred_api_resource_group,
         nasdaq_quandl_api_resource_group,
         oanda_api_resource_group,
+        twelvedata_api_resource_group,
     ]
     return {rg.identifier: rg for rg in resource_groups}
